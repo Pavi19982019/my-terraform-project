663b558b1378cafb56fe361f90194dcf48eb4c9a
Pavi19982019 <90970424+Pavi19982019@users.noreply.github.com>
2026-01-31 12:25:49 +0530

    MF

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "exmaple" {
-  bucket = "my-unique-test-bucket-12345_by_pavithra"
+resource "aws_s3_bucket" "example" {
+  bucket = "my-unique-test-bucket-12345-by-pavithra"
 }
